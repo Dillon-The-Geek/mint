8fa7f80db62d9d753b1142cdbf894d8c3bab390a
Dillon-The-Geek <113472229+Dillon-The-Geek@users.noreply.github.com>
2023-12-12 13:00:25 +0000

    Add files via upload

---
+++ post-install-script.sh
@@ -0,0 +1,46 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo systemctl enable ufw.service --now
+
+sudo apt install rofi
+
+sudo apt install shotcut
+
+sudo apt install obs-studio
+
+sudo apt install curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
+
+sudo mv /etc/apt/preferences.d/nosnap.pref ~/Documents/nosnap.backup
+
+sudo apt update
+
+sudo apt install snapd
+
+snap install discord
+
+snap install spotify
+
+sudo apt install virt-manager
+
+sudo adduser $USER libvirt
+
+sudo adduser $USER libvirt-qemu
+
+sudo apt install pavucontrol
+
+sudo apt install steam
+
+sudo apt install git
+
+git clone https://github.com/AdnanHodzic/auto-cpufreq.git
+cd auto-cpufreq && sudo ./auto-cpufreq-installer
+
+sudo apt install pavucontrol
